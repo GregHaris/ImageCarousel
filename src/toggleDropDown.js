c11d809f5f1685e2f7b7c73055e94cf55498dfd3
Grëg Häris <d.gregharis@gmail.com>
2024-09-02 15:29:49 +0100

    deleted: file not needed for assignment

--- src/toggleDropDown.js
+++
@@ -1,9 +0,0 @@
-import { cacheDOM } from "./domUtils";
-import { showDropDown, hideDropDown } from "./eventHandlers";
-
-const { services, dropdownList } = cacheDOM();
-
-export function toggleDropDown() {
-  services.addEventListener("click", showDropDown);
-  dropdownList.addEventListener("mouseleave", hideDropDown);
-}
